653068a8b500df562f6cdf0031de25b62990f26e
Jon McDonald <Jon.McDonald@ibm.com>
2022-11-04 13:57:40 -0500

    initial checking of template engine class

---
+++ src/main/java/com/metlife/buddy/alertprocessor/utils/BuddyTemplateEngine.java
@@ -0,0 +1,35 @@
+package com.metlife.buddy.alertprocessor.utils;
+import java.io.StringWriter;
+ 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+public class BuddyTemplateEngine {
+
+    public String buildAlertNotification()
+    {
+        String alertTemplatePath = System.getenv("alertTemplatePath");
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+         
+        Template t = ve.getTemplate(alertTemplatePath);
+         
+        VelocityContext vc = new VelocityContext();
+        vc.put("empname", "John Doe");
+        vc.put("standardhours", "60");
+        vc.put("bookedhours", "88");
+        vc.put("buddyowner", "Rudy Daniels");
+
+             
+        StringWriter sw = new StringWriter();
+        t.merge(vc, sw);
+
+        System.out.println(sw);
+
+        return sw.toString();
+         
+        
+    }
+    
+}
